fd0fb6d57c3f57c2121fe4101a03b0b3c3d79e75
Lisen <SpritLisen@gmail.com>
2025-08-18 11:36:34 +0800

    add: 增加 PYMO 脚本的解析逻辑

--- script_parse.py
+++ script_parse.py
@@ -83,5 +83,88 @@ def parse_ons_script(ons_script_path):
     print("字符列表:", unique_chars)
 
 
+def parse_pymo_script(pymo_script_path):
+    file_path = Path(pymo_script_path)
+
+    output_path = file_path.with_name(output_dir)
+    output_file_path = output_path / "pymo_version.txt"
+    output_file = open(output_file_path, "w", encoding="utf-8")
+
+    # 全屏一行渲染的文案内容
+    screen_msgs = []
+    # 单独的对白文案
+    says = []
+    # 人物名称
+    persons = []
+    # 人物对白文案
+    person_says = []
+
+    # 1. b__screenmsg: 抓取第一个 "" 中的内容
+    re_screen_msg = re.compile(r'^b__screenmsg.*?"(.*?)"')
+    # 2. #say content || #say 【name】,「content」
+    re_say = re.compile(r'^#say\s+([^,\s]+)(?:,([^,\s]+))?')
+
+    # 解析实际内容
+    def parse_content(files):
+        for main_script in files:
+            with open(main_script, "r", encoding="utf8", errors="ignore") as f:
+                for line in f:
+                    line = line.strip()
+
+                    # 匹配 b__screenmsg
+                    m1 = re_screen_msg.match(line)
+                    if m1:
+                        screen_msg = m1.group(1)
+                        screen_msgs.append(screen_msg)
+
+                        output_file.write(screen_msg + "\n")
+                        continue
+
+                    m2 = re_say.match(line)
+                    if m2:
+                        # 匹配 #say 【name】,「content」
+                        if m2.group(2):
+                            person_name = m2.group(1)
+                            person_say = m2.group(2)
+                            person_says.append(person_say)  # 对白
+                            persons.append(person_name)  # 人物名
+
+                            output_file.write(person_name + "\n")
+                            output_file.write(person_say + "\n")
+                            continue
+                        # 匹配 #say content
+                        else:
+                            say = m2.group(1)
+                            says.append(say)
+
+                            output_file.write(say + "\n")
+                            continue
+
+    # 解析主剧情
+    main_script_path = file_path / "main"
+    parse_content(main_script_path.glob('*.[Tt][Xx][Tt]'))
+
+    # 解析附录剧情
+    appendix_script_path = file_path / "appendix"
+    parse_content(appendix_script_path.glob('*.[Tt][Xx][Tt]'))
+
+    # 解析终末话语
+    parse_content(file_path.glob('*.[Tt][Xx][Tt]'))
+
+    output_file.close()
+
+    # 合并所有文本
+    all_text = "".join(screen_msgs + says + persons + person_says)
+
+    # 统计字符集合
+    # \u3000 是全角的空格
+    unique_chars = sorted(set(all_text))
+
+    # 输出
+    print("总字符数:", len(unique_chars))
+    print("字符列表:", unique_chars)
+
+
 if __name__ == "__main__":
     parse_ons_script("game_script/ons_version.txt")
+    parse_pymo_script("game_script/pymo")
